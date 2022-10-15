6b63e412ff456c4767fdea4e8043edbff0aa6027
PlatFormPlayZ <zjrw@icloud.com>
2022-10-15 15:38:42 -0500

    pause menu

--- Trash_Pick-Up_Simulator/Assets/Scripts/PauseMenu.cs
+++ Trash_Pick-Up_Simulator/Assets/Scripts/PauseMenu.cs
@@ -6,21 +6,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
 public class PauseMenu : MonoBehaviour
 {
-    private GameObject pauseMenu;
-    private GameObject controlsMenu;
-    private bool pauseOpen = false;
-    private bool controlsOpen = false;
+    public GameObject pauseMenu;
+    public GameObject controlsMenu;
+    public bool pauseOpen = false;
+    public bool controlsOpen = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
-        controlsMenu = GameObject.FindGameObjectWithTag("ControlsMenu");
+        //pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
+        //controlsMenu = GameObject.FindGameObjectWithTag("ControlsMenu");
         pauseMenu.SetActive(false);
         controlsMenu.SetActive(false);
     }
@@ -30,25 +31,23 @@ public class PauseMenu : MonoBehaviour
     {
         
         //if screen is not open, is set to active when player presses escape key and pauses the game
-        if (!pauseOpen && Input.GetKeyDown(KeyCode.Escape))
+        if (!pauseOpen && (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)))
         {
-
             //open pause menu
             pauseOpen = true;
             pauseMenu.SetActive(true);
             Cursor.visible = true;
         }
         //if is open and player presses escape, hides pause menu and resumes game
-        if (pauseOpen && Input.GetKeyDown(KeyCode.Escape))
+        else if (pauseOpen && (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)))
         {
-
             //open pause menu
             pauseOpen = false;
             pauseMenu.SetActive(false);
             Cursor.visible = false;
         }
         //controls escape button on controls menu
-        if (controlsOpen && Input.GetKeyDown(KeyCode.Escape))
+        else if (controlsOpen && (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)))
         {
             controlsMenu.SetActive(false);
         }
